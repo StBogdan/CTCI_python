c7942238fdee412e6855ca4a3b18579932be0c03
StBogdan <bogdan.stoicescu95@gmail.com>
2021-01-02 14:55:24 +0000

    Add a better method for p10_9

--- chapter_10/p10_9.py
+++ chapter_10/p10_9.py
@@ -1,79 +1,114 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
+import typing
 
 
-def matrix_bin_search(arr: List[List[int]], t: int) -> Optional[tuple]:
+# Method: Search space reduction
+# Time: O(m + n), where m, n dimensions of input matrix
+# Space: O(1)
+
+def get_at(arr: List[List[int]], point: Tuple[int, int]) -> int:
+    px, py = point
+    return arr[px][py]
+
+
+def matrix_search(arr: List[List[int]], target: int) -> Optional[tuple]:
+    rows = len(arr)
+    cols = len(arr[0])
+
+    row_now = rows-1
+    col_now = 0
+    while 0 < row_now and col_now < cols:
+        elem_now = get_at(arr, (row_now, col_now))
+        # print(f"Looking at {row_now:3}:{col_now:3}, of elem {elem_now}")
+
+        if elem_now == target:
+            return row_now, col_now
+        elif elem_now < target:
+            col_now += 1
+        else:
+            row_now -= 1
+
+
+# The following functions were used in the old version (looking in quadrants)
+# An example of the Master theorem for figuring out complexity
+
+
+def matrix_bin_search_quadrants(arr: List[List[int]], t: int) -> Optional[tuple]:
     n = len(arr)-1
     m = len(arr[0])-1
     return mbs(arr, 0, n, 0, m, t)
 
 
-def gat(arr, point):
-    px, py = point
-    return arr[px][py]
-
-
-def check_square(a, xl, xh, yl, yh, t) -> Optional[tuple]:
-    poss = [(x, y) for x in (xl, xh) for y in (yl, yh)]
-    for poz in poss: 
-        if gat(a, poz) == t:
+def check_square(matrix, x_low, x_high, y_low, y_high, target) -> Optional[tuple]:
+    checking_positions = [(x, y) for x in (x_low, x_high)
+                          for y in (y_low, y_high)]
+    for poz in checking_positions:
+        if get_at(matrix, poz) == target:
             return poz
 
 
-def mbs(a, xl, xh, yl, yh, t):
-    print(f"Looking within [x:{xl}, y: {yl}]\t-> [x:{xh}, y:{yh}]")
+def mbs(matrix, 
+        x_low, x_high, 
+        y_low, y_high, 
+        target):
+    print(
+        f"Looking within [x:{x_low}, y: {y_low}]\t-> [x:{x_high}, y:{y_high}]")
 
-    if xl > xh or yl > yh:
+    if x_low > x_high or y_low > y_high:
         return None
 
-    xm = (xl + xh) // 2
-    ym = (yl + yh) // 2
+    x_mid = (x_low + x_high) // 2
+    y_mid = (y_low + y_high) // 2
 
-    upp = (xl, ym)
-    leftp = (xm, yl)
+    up_point = (x_low, y_mid)
+    left_point = (x_mid, y_low)
 
-    mmp = (xm, ym)
+    mid_point = (x_mid, y_mid)
 
-    if t == gat(a, upp):
-        return upp
-    elif t == gat(a, leftp):
-        return leftp
-    elif t == gat(a, mmp):
-        return mmp
+    if target == get_at(matrix, up_point):
+        return up_point
+    elif target == get_at(matrix, left_point):
+        return left_point
+    elif target == get_at(matrix, mid_point):
+        return mid_point
 
     # Lower right
-    if t > gat(a, mmp):
-        print(f"Going low right with new mmp {mmp}")
-        if mmp[0] == xl and mmp[1] == yl:
-            return check_square(a, xl, xh, yl, yh, t)
+    if target > get_at(matrix, mid_point):
+        # print(f"Going low right with new mid-mid-point {mmp}")
+        if mid_point[0] == x_low and mid_point[1] == y_low:
+            return check_square(matrix, x_low, x_high, y_low, y_high, target)
 
-        return mbs(a, mmp[0], xh, mmp[1], yh, t)
+        return mbs(matrix,
+                   mid_point[0], x_high,
+                   mid_point[1], y_high,
+                   target)
 
     # Top right and lower left
-    if t > gat(a, upp):
+    if target > get_at(matrix, up_point):
         print("Going top left")
-        r1 = mbs(a, xl, mmp[0]-1,
-                 upp[1], yh, t)
+        r1 = mbs(matrix, x_low, mid_point[0]-1,
+                 up_point[1], y_high, target)
         if r1:
             return r1
-    if t > gat(a, leftp):
+    if target > get_at(matrix, left_point):
         print("Going bot right")
-        r2 = mbs(a, leftp[0], xh,
-                 yl, mmp[1]-1, t)
+        r2 = mbs(matrix, left_point[0], x_high,
+                 y_low, mid_point[1]-1, target)
 
         if r2:
             return r2
 
     # Top left
-    print("Going top left")
-    if not (t > gat(a, upp) or t > gat(a, leftp)):
-        return mbs(a, xl, mmp[0]-1, yl, mmp[1]-1, t)
+    # print("Going top left")
+    if not (target > get_at(matrix, up_point) or target > get_at(matrix, left_point)):
+        return mbs(matrix, x_low, mid_point[0]-1, y_low, mid_point[1]-1, target)
 
 
 if __name__ == "__main__":
     example1 = [[0, 5, 6, 7, 8],
                 [1, 6, 7, 8, 9],
-                [2, 10, 15, 120, 21],
+                [2, 10, 15, 12, 21],
                 [3, 11, 16, 21, 22],
                 [4, 12, 20, 70, 90]]
 
-    print(matrix_bin_search(example1, 21))
+    print(matrix_bin_search_quadrants(example1, 21), matrix_search(example1, 21))
